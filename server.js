37e039a21e32d35dfede2cf296ea61dd70f2b3a7
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-24 14:07:07 +1300

    text version of the score API

--- server.js
+++ server.js
@@ -70,6 +70,60 @@ app.post('/', function(req, res) {
   });
 });
 
+function scoreToCSV(items) {
+  var lines = items.map(function (item, i) {
+    return item.score + ',' + item.player;
+  });
+
+  lines.sort(function(a, b) {
+    a = parseInt(a.split(',')[0], 10);
+    b = parseInt(b.split(',')[0], 10);
+    return a > b ? -1 : (a === b ? 0 : 1);
+  });
+
+  return lines.join('\n');
+}
+
+// curl -X POST --data "player=awesome&score=30000&gist=gisturl" http://localhost:5000/scores.txt
+app.post('/scores.txt', function(req, res) {
+  addToList(req.body, function(err) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.type('text/plain');
+    if (err) {
+      res.status(500).send(err.message || 'Undefined error');
+    } else {
+      var options = {
+        order: req.query.reverse ? -1 : 1,
+        limit: req.query.limit || 10
+      };
+      getList(options, function(err, items) {
+        if (err) {
+          res.status(400).send(err.message || 'Undefined error');
+        } else {
+          res.send(scoreToCSV(items));
+        }
+      });
+    }
+  });
+});
+
+// curl http://localhost:5000/scores.txt
+app.get('/scores.txt', function(req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.type('text/plain');
+  var options = {
+    order: req.query.reverse ? -1 : 1,
+    limit: req.query.limit || 10
+  };
+  getList(options, function(err, items) {
+    if (err) {
+      res.status(400).send(err.message || 'Undefined error');
+    } else {
+      res.send(scoreToCSV(items));
+    }
+  });
+});
+
 app.get('/hello', function(request, response) {
   response.send('Hello World!');
 });
